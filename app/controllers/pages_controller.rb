768b9b3b09fb6e973180f10a7e22cdec2409245c
Kabbaj <mr.kabbaj.hassan@gmail.com>
2016-03-29 00:01:25 +0100

    Created the dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+  # make sure users are logged in.
+  before_action :authenticate_user!, only: [:dashboard]
+
   def home
   	if current_user 
         redirect_to books_path
     end
+  end
+
+  def dashboard
+  	@books = current_user.books
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   #get 'pages/home'
 
   root 'pages#home'
+  get 'dashboard' => 'pages#dashboard'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
